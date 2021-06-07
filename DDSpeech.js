7f96a2c4bd5bb056ef8873d08c40e1fc13b256c6
Dennie <dennie@ddsoft.be>
2021-06-07 14:56:19 +0000

    searching.. seek and;. :)

--- DDSpeech.js
+++ DDSpeech.js
@@ -2,21 +2,61 @@
   var phraseDiv;
   var resultDiv;
   var readAloudButton;
+  var DDText;
 
   // subscription key and region for speech services.
   var subscriptionKey, serviceRegion;
   var SpeechSDK;
   var synthesizer;
 
-  document.addEventListener("DOMContentLoaded", function (){
-    readAloudButton = document.getElementById("readAloudButton");
-    resultDiv = document.getElementById("resultDiv");
 
+ function onload()
+  {
+    if (!!window.SpeechSDK) {
+      SpeechSDK = window.SpeechSDK;
+      readAloudButton.disabled = false;
+    }
+   // resultDiv.innerHTML = "Loaded";
+    document.getElementById("resultDiv").innerHTML = "Loaded";
+  }  
+
+  function readAloud()
+  {
     subscriptionKey = prompt("Subscription key: ");
-    resultDiv.innerHTML = subscriptionKey;
-  }
-  )
+    serviceRegion = "westeurope";
+    document.getElementById("resultDiv").innerHTML = subscriptionKey;
+    var welcome = document.getElementById("welcomeHead").innerText;
+    document.getElementById("resultDiv").innerHTML += "<br/>" + welcome;
+    DDText = welcome;
+    var speechConfig = SpeechSDK.SpeechConfig.fromSubscription(subscriptionKey.value, serviceRegion.value);
 
+    synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig);
+    synthesizer.speakTextAsync(
+        DDText,
+      function (result) {
+        readAloudButton.disabled = false;
+        if (result.reason === SpeechSDK.ResultReason.SynthesizingAudioCompleted) {
+          resultDiv.innerHTML += "synthesis finished for [" + inputText + "].\n";
+        } else if (result.reason === SpeechSDK.ResultReason.Canceled) {
+          resultDiv.innerHTML += "synthesis failed. Error detail: " + result.errorDetails + "\n";
+        }
+        window.console.log(result);
+        synthesizer.close();
+        synthesizer = undefined;
+      },
+      function (err) {
+        readAloudButton.disabled = false;
+        resultDiv.innerHTML += "Error: ";
+        resultDiv.innerHTML += err;
+        resultDiv.innerHTML += "\n";
+        window.console.log(err);
+
+        synthesizer.close();
+        synthesizer = undefined;
+    });
+  }
+  
+   
 /*
   document.addEventListener("DOMContentLoaded", function () {
     startSpeakTextAsyncButton = document.getElementById("startSpeakTextAsyncButton");
@@ -78,5 +118,5 @@
       }
     }
   });
-
-  */
+*/
+  
